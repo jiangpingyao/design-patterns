2bb2a9cd56f468120c4d0ba1765a38ee7cd6547f
jiangpingyao <1663946322@qq.com>
2018-07-29 15:19:01 +0800

    设计模式之工厂模式

--- src/main/patterns/factory/abstr/AbstractFactoryTest.java
+++ src/main/patterns/factory/abstr/AbstractFactoryTest.java
@@ -1,8 +1,6 @@
 package main.patterns.factory.abstr;
 
-/**
- * 抽象工厂模式
- */
+
 public class AbstractFactoryTest {
     public static void main(String[] args) {
         System.out.println(new CarFactory().getAodi());
